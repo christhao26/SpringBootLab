4062ef54970c20913dcbc95759d8440383cbc2e9
christhao26 <79154880+christhao26@users.noreply.github.com>
2021-05-13 16:10:18 -0400

    Initial commit

---
+++ src/main/java/com/tts/demo/NewClass.java
@@ -0,0 +1,21 @@
+package com.tts.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class NewClass {
+
+    @GetMapping("/newFile")
+    public Integer ourFunction() {
+        return 5+6;
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
